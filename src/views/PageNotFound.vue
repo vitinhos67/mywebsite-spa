6ee61d8efaf4b72a99171d3f4be9f5a3245934a3
Victor Santos <santtosvictor42@gmail.com>
2023-10-26 16:05:50 -0300

    chore: removed unnecessary files and fix 404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -2,13 +2,6 @@
   <div class="container-404">
     <p class="text-bird">Esta perdido?</p>
 
-    <img
-      class="animaiszinhos"
-      src="../assets/images/animaiszinhos.png"
-      alt="Animaiszinhos legais e fofinhos. Sendo gatos, cachorros e passaros, e
-        também um ratinho"
-    />
-
     <a href="/">
       <button class="button-back-page">Voltar para pagina principal</button></a
     >
@@ -22,16 +15,14 @@ export default {
 </script>
 
 <style scoped>
-.text-bird {
-  font-family: "Comic Sans MS", "Comic Sans", sans-serif;
-  font-size: 30px;
-  text-align: center;
-}
-
 .container-404 {
   text-align: center;
   padding-top: 40px;
   height: 620px;
+}
+
+p {
+  margin: 20px;
 }
 
 img.animaiszinhos {
@@ -39,27 +30,16 @@ img.animaiszinhos {
   margin: 30px;
 }
 
-.h1-404 {
-  font-size: 180px;
-  color: rgb(231, 15, 15);
-}
-
-a {
-  text-decoration: none;
-}
-
-a:hover {
-  color: green;
-}
 .button-back-page {
   display: block;
   text-decoration: none;
   margin: auto;
   color: #ffff;
-  padding: 20px;
+  padding: 10px;
   font-size: 20px;
   background-color: #01004c;
   border-radius: 10px;
+  border: none;
   color: #ffffff;
   transition: background-color 0.4s ease, box-shadow 0.5s ease;
 }
